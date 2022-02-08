f876c1466500cf2b9379d094cad305f2e6c7d624
camilla <cgreene553@gmail.com>
2022-02-08 15:20:05 +0000

    Sinatra introducing  params

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+#attr_reader :cat_name
+
 get '/' do
   "hello!"
 end
@@ -13,6 +15,13 @@ get '/makers' do
   "makers"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @cat_name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  @name = params[:name]
   erb(:index)
 end
